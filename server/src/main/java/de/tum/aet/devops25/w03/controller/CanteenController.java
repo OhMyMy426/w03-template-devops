6d851e84d5bf3cb78bf7dfa76a4c49741d82ef95
Xiyu <767145420@qq.com>
2025-05-09 13:34:23 +0200

    done

--- server/src/main/java/de/tum/aet/devops25/w03/controller/CanteenController.java
+++ server/src/main/java/de/tum/aet/devops25/w03/controller/CanteenController.java
@@ -20,6 +20,7 @@ public class CanteenController {
 
     /**
      * Get today's meals for a specific canteen
+     *
      * @param canteenName the ID of the canteen (e.g., "mensa-garching")
      * @return list of dishes available today at the specified canteen
      */
@@ -27,6 +28,10 @@ public class CanteenController {
     public ResponseEntity<List<Dish>> getTodayMeals(@PathVariable String canteenName) {
         // TODO: Implement the logic to fetch today's meals for the specified canteen using the CanteenService
         List<Dish> todayMeals = canteenService.getTodayMeals(canteenName);
+
+        if (todayMeals.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
         return ResponseEntity.ok(todayMeals);
     }
 }
